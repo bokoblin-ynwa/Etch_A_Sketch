33e6bd6a973cc50969881ec663baa1e5ed6be82e
bokoblin-ynwa <212385897+bokoblin-ynwa@users.noreply.github.com>
2025-08-06 21:34:07 -0500

    deleted CSS content and styled from the JS file. Created grid function(needs adjustments)' ' '

--- script.js
+++ script.js
@@ -1,13 +1,21 @@
 const gridContainer = document.getElementById('container');
-const rows = 16;
-const cols = 16;
+gridContainer.setAttribute("style", "height: 600px; width: 600px; background-color:rgb(94, 233, 246); display: flex; flex-direction: colum; align-items: center; margin: auto; border: 7px solid black;");
 
-for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-        const gridItem = document.createElement('div');
-        gridItem.classList.add('grid-item');
-        gridContainer.appendChild(gridItem);
+
+function createGrid(container, rows = 16, cols = 16) {
+    const cellContainer = document.getElementById('container');
+    for(let i = 0; i < rows * cols; i++) {
+        const cell = document.createElement('div');
+        cellContainer.style.display = 'grid';
+        cellContainer.style.gridTemplateColumns = `repeat(${cols}, 1fr)`;
+        cellContainer.style.gridTemplateRows = `repeat(${ rows}, 1fr)`;
+        cellContainer.style.width = '600px';
+        cellContainer.style.height = '600px';
+        cell.classList.add('grid-item');
+       /* cell.style.border = 'black solid 2px';*/    // Border added to confirm cells were created
+        cellContainer.appendChild(cell);
+        
     }
 }
 
-const pixles = document.getElementsByClassName("")
+createGrid('container');
